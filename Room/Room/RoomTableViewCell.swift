89d7143c734eba69f0b4c83d6028c3291685d0ea
wndzlf <wndzlf@gmail.com>
2018-01-24 18:21:21 +0900

    1/24 roomData

---
+++ Room/Room/RoomTableViewCell.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+
+class RoomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var RoomImageView : UIImageView!
+    @IBOutlet weak var RoomtitleLabel : UILabel!
+    @IBOutlet weak var RoomNoticeLabel : UILabel!
+    
+
+}
