cf22b39d0d2a62750048f75632ff116094f2d147
wndzlf <wndzlf@gmail.com>
2018-01-26 14:25:34 +0900

    문제 출제

---
+++ HOF_Teacher.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let heros = ["Ironman", "Batman", "BlckWidow", "Tor", "CaptainAmerica", "Spiderman"]
+//1.대문자 소문자 바꾸기
+//2.H이상인것 출력하기. ---> 예시 올려드림
+//3.사전식 배열하기
+//4.세자리수 짝수 더하기
+//5.앞뒤가 대문자인 원소 출력
+
+
